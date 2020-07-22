a47f6f61ae666641f309930ac21f312772350da0
write2vaibhav <39696872+write2vaibhav@users.noreply.github.com>
2020-07-22 18:43:47 +0530

    Add files via upload

---
+++ Day2 Assignment.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("hello world")
+
+
+# In[6]:
+
+
+print("""  (=‘.’=)
+  (“)_(”)    """)
+
+
+# In[ ]:
+
+
+""" Creating doc string"""
+
+
+# In[7]:
+
+
+print("Python's programme")
+
+
+# In[8]:
+
+
+print("Hello \n Python")
+
+
+# In[10]:
+
+
+print("Python\'s Programme")
+
+
+# In[11]:
+
+
+print("Hello\t python")
+
+
+# In[14]:
+
+
+name = "XYZ"
+marks = "90"
+age = "20"
+print("The name of person is ", name, "Marks is", marks, "Age is", age)
+
+
+# In[18]:
+
+
+name = "XYZ"
+marks = 90.8976
+age = 20
+print("The name of person is %s Marks is %f Age is %d" %(name, marks, age))
+
+
+# In[19]:
+
+
+name = "XYZ"
+marks = 90.8976
+age = 20
+print("The name of person is %4s Marks is %.2f Age is %d" %(name, marks, age))
+
+
+# In[21]:
+
+
+name = "XYZ"
+marks = 90.8976
+age = 20
+print(f"The name of person is {name} Marks is {marks} Age is {age}")
+
+
+# In[22]:
+
+
+a = 5**5
+print(a)
+
+
+# In[23]:
+
+
+a = 5
+b = 6
+d = a/b
+e = a//b
+print(d)
+print(e)
+
+
+# In[24]:
+
+
+a = 10
+b = 10
+a == b
+
+
+# In[25]:
+
+
+a = 10
+A = 10
+print(id(a))
+print(id(A))
+
+
+# In[36]:
+
+
+a = 10
+b = 20
+print(bin(a))
+print(bin(b))
+print(a|b)
+
+
+# In[27]:
+
+
+10 > 9 and 20 > m
+
+
+# In[28]:
+
+
+10 < 9 and 20 > m
+
+
+# In[30]:
+
+
+str1  = "pythonprogramming"
+"x" in str1
+
+
+# In[31]:
+
+
+str1  = "pythonprogramming"
+"x" not in str1
+
+
+# In[34]:
+
+
+a = 10
+b = 10
+print(a == b)
+print(a is b)
+
+
+# In[35]:
+
+
+a = 257
+b = 257
+print(a == b)
+print(a is b)
+
+
+# In[37]:
+
+
+10>8>9
+
+
+# In[ ]:
+
+
+
+
